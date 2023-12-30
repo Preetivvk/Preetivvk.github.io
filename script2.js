4a5ec43505a4c18b4485ac67cd24e0f274748afc
PREETI <97424573+Preetivvk@users.noreply.github.com>
2023-12-30 22:34:40 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,38 @@
+
+function toggleMobileMenu() {
+  var linksContainer = document.getElementById("myLinks");
+  linksContainer.style.display = (linksContainer.style.display === "block") ? "none" : "block";
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function toggleNav() {
+  var links = document.getElementById("myLinks");
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
